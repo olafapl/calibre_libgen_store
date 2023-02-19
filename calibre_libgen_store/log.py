427a479d11d8afdc70a963f80586848be08827a6
Olaf Apelseth Liadal <olaf.liadal@gmail.com>
2023-02-19 15:46:10 +0100

    Remove unused constant

--- calibre_libgen_store/log.py
+++ calibre_libgen_store/log.py
@@ -2,7 +2,6 @@ import sys
 import logging
 
 NAME = "Library Genesis store"
-LOG_LEVEL = logging.DEBUG
 
 formatter = logging.Formatter(fmt=f"%(asctime)s [%(levelname)s] {NAME} - %(message)s")
 handler = logging.StreamHandler(sys.stdout)
