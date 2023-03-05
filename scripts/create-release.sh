a5b52e1f4aa4949e078f7d7e3533da94a5ab5317
Olaf Apelseth Liadal <olaf.liadal@gmail.com>
2023-03-05 16:07:20 +0100

    Update release script

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/env sh
 
 VERSION=$(grep -Po "version =\s\K.*" pyproject.toml)
-echo "__version__ = $VERSION" > calibre_libgen_store/__version__.py
 
 cp -a calibre_libgen_store/. .cache/release
 cp LICENSE .cache/release
+echo "__version__ = $VERSION" > calibre_libgen_store/__version__.py
 echo "from .store import LibGenStore" > .cache/release/__init__.py
 
 mkdir -p dist
-zip -rj dist/calibre_libgen_store-$(echo $VERSION | tr -d '"').zip .cache/release -i /*.py
+zip -j dist/calibre_libgen_store-$(echo $VERSION | tr -d '"').zip .cache/release/*
